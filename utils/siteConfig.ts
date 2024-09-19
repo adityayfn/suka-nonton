ca6cd922752ff60e956da2e468f7d1b2e709c411
adityayfn <adhitya.firstian@gmail.com>
2024-09-19 13:59:50 +0700

    update:: change scrap url

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,5 +1,6 @@
 export const siteConfig = {
-  scrapUrl: "https://www.ragihospitalkukatpally.com",
+  scrapUrl: "https://www.eatfamoso.com",
+  // "https://www.ragihospitalkukatpally.com",
   // scrapUrl: "https://www.greatlaketolaketrail.org",
 }
 
