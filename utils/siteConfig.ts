9ed73df8a2012470167403b3c838167aea2ab4b3
aditya yudha <69260395+adityayfn@users.noreply.github.com>
2024-11-25 18:59:49 +0700

    Update siteConfig.ts

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,5 +1,6 @@
 export const siteConfig = {
-  scrapUrl: "https://ics-hs.com", 
+  scrapUrl: "https://bribristrong.com"
+    //"https://ics-hs.com", 
     // "https://www.eatfamoso.com",
   // "https://www.ragihospitalkukatpally.com",
   // scrapUrl: "https://www.greatlaketolaketrail.org",
