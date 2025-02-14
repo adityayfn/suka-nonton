2f68a98f68ebc7d63328a9493ffffda159879c83
adityayfn <adhitya.firstian@gmail.com>
2025-02-14 15:26:21 +0700

    update siteConfig

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,8 +1,9 @@
 export const siteConfig = {
-  scrapUrl: "https://www.torontofoodfilmfest.com"
-    //"https://bribristrong.com"
-    //"https://ics-hs.com", 
-    // "https://www.eatfamoso.com",
+  scrapUrl: "https://surgerycenterattanasbourne.com",
+  // "https://www.torontofoodfilmfest.com"
+  //"https://bribristrong.com"
+  //"https://ics-hs.com",
+  // "https://www.eatfamoso.com",
   // "https://www.ragihospitalkukatpally.com",
   // scrapUrl: "https://www.greatlaketolaketrail.org",
 }
