6d0ca1f01d7d775fefbf164f7acef88934cf5032
adityayfn <adhitya.firstian@gmail.com>
2025-08-19 22:17:43 +0700

    .

--- server/api/movies/detail/[...movie].ts
+++ server/api/movies/detail/[...movie].ts
@@ -23,7 +23,13 @@ export default defineEventHandler(async (event) => {
   const dataMovie: { [key: string]: string } = {}
 
   try {
-    const res = await axios.get(url)
+    const res = await axios.get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/126.0 Safari/537.36",
+        Accept: "text/html,application/xhtml+xml",
+      },
+    })
 
     const $ = cheerio.load(res.data)
 
