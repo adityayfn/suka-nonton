c7ab22a574e8d8efc4b4b9ff094584b2d29ee6f2
adityayfn <adhitya.firstian@gmail.com>
2025-08-19 21:56:20 +0700

    update site url

--- utils/siteConfig.ts
+++ utils/siteConfig.ts
@@ -1,11 +1,5 @@
 export const siteConfig = {
-  scrapUrl: "https://surgerycenterattanasbourne.com",
-  // "https://www.torontofoodfilmfest.com"
-  //"https://bribristrong.com"
-  //"https://ics-hs.com",
-  // "https://www.eatfamoso.com",
-  // "https://www.ragihospitalkukatpally.com",
-  // scrapUrl: "https://www.greatlaketolaketrail.org",
+  scrapUrl: "https://new16.ngefilm.site",
 }
 
 // https://www.ngefilm21.pw/
